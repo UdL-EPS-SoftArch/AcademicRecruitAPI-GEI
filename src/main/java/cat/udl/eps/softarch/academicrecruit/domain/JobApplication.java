3d34407d186054405a7500ad701a703e21941c2f
Roberto García <roberto@rhizomik.net>
2021-03-09 16:06:43 +0100

    Annotate fields instead of methods No need for getters/setter using Lombok

--- src/main/java/cat/udl/eps/softarch/academicrecruit/domain/JobApplication.java
+++ src/main/java/cat/udl/eps/softarch/academicrecruit/domain/JobApplication.java
@@ -3,63 +3,22 @@ package cat.udl.eps.softarch.academicrecruit.domain;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "JobApplication") //Avoid collision with system table User in Postgres
 @Data
 public class JobApplication {
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public List<String> getRequirements() {
-        return requirements;
-    }
-
-    public void setRequirements(List<String> requirements) {
-        this.requirements = requirements;
-    }
-
-    public List<String> getEvaluationCriteria() {
-        return evaluationCriteria;
-    }
-
-    public void setEvaluationCriteria(List<String> evaluationCriteria) {
-        this.evaluationCriteria = evaluationCriteria;
-    }
-
+    @Id
     private String id;
     private String name;
     private String description;
     @ElementCollection
-    private List<String> requirements;
+    private List<String> requirements = new ArrayList<>();
     @ElementCollection
-    private List<String> evaluationCriteria;
+    private List<String> evaluationCriteria = new ArrayList<>();
     //private Phase currentPhase;
     //private List<Phase> pases;
     //private List<Applicants> applicants;
     //private List<Committee> committees;
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    @Id
-    public String getId() {
-        return id;
-    }
-
 }
