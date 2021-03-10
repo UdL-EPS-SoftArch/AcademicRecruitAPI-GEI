aafd3a9b07692eea564d13e10cca6f8ae399f633
Victor Alcobe <victor.alcobe@usm.cl>
2021-03-10 19:07:44 +0100

    add class

--- src/main/java/cat/udl/eps/softarch/academicrecruit/domain/Qualifications.java
+++ src/main/java/cat/udl/eps/softarch/academicrecruit/domain/Qualifications.java
@@ -3,20 +3,35 @@ package cat.udl.eps.softarch.academicrecruit.domain;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
-import com.fasterxml.jackson.annotation.JsonValue;
+
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import org.hibernate.validator.constraints.Length;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Collection;
 
-public class Qualifications {
+import lombok.Value;
+
+
+@Entity
+@Data
+@Value
+
+public class Qualifications extends UriEntity<Long>{
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    private String observation;
+
+    @NotBlank
+    @Min(0)
+    @Max(10)
+    private Float mark;
+
+
+
 }
