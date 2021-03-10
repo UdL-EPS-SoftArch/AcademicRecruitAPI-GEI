7e3a92103e86f1b09e3ba744356bd39294461178
Marc Saureu <marcsaureu@gmail.com>
2021-03-10 17:25:14 +0100

    NotBlank Anotiation Removed

--- src/main/java/cat/udl/eps/softarch/academicrecruit/domain/User.java
+++ src/main/java/cat/udl/eps/softarch/academicrecruit/domain/User.java
@@ -43,13 +43,10 @@ public class User extends UriEntity<String> implements UserDetails {
     @Length(min = 8, max = 256)
     private String password;
 
-    @NotBlank
     private String name;
 
-    @NotBlank
     private String lastname;
 
-    @NotBlank
     @Column(unique = true)
     @Length(min = 9, max = 9)
     private String dni;
@@ -93,49 +90,5 @@ public class User extends UriEntity<String> implements UserDetails {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER");
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getDni() {
-        return dni;
-    }
-
-    public void setDni(String dni) {
-        this.dni = dni;
     }
 }
