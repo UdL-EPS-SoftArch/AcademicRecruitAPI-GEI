b7e7aff4ea24ba99a0d3626aa483a268fb6a690f
Victor Alcobe <victor.alcobe@usm.cl>
2021-03-10 18:30:46 +0100

    Class added

---
+++ src/main/java/cat/udl/eps/softarch/academicrecruit/domain/Qualifications.java
@@ -0,0 +1,22 @@
+package cat.udl.eps.softarch.academicrecruit.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Collection;
+
+public class Qualifications {
+}
