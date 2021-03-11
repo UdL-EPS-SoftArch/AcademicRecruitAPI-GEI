bfda98a7f91c50af0fbad8acb73211c0ed392565
Marc Saureu <marcsaureu@gmail.com>
2021-03-11 16:06:07 +0100

    Applicants class created

---
+++ src/main/java/cat/udl/eps/softarch/academicrecruit/domain/Applicants.java
@@ -0,0 +1,35 @@
+package cat.udl.eps.softarch.academicrecruit.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Applicants extends UriEntity<Long> {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    @NotBlank
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String lastname;
+
+    @NotBlank
+    @Column(unique = true)
+    @Length(min = 9, max = 9)
+    private String dni;
+
+}
