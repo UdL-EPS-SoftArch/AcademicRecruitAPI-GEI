a2a7c5546b73647d9b96c260eb4f210da5417b9c
Paula (Latra) Gallucci <personadelmonton@gmail.com>
2021-03-11 16:04:01 +0100

    Añadida la clase Phase y su repositorio

---
+++ src/main/java/cat/udl/eps/softarch/academicrecruit/domain/Phase.java
@@ -0,0 +1,26 @@
+package cat.udl.eps.softarch.academicrecruit.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Phase extends UriEntity<Long>{
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    private Integer phaseNumber;
+    private String name;
+    private Date initialDate;
+    private Date finalDate;
+
+
+}
