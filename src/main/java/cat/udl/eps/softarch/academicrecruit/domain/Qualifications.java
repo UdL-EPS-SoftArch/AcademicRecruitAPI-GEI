dc9355a65c05d2c505f5c76384f9fdbee9750e20
Xavier <xaavier@protonmail.com>
2021-03-16 16:30:35 +0100

    404 error on Qualifications test

--- src/main/java/cat/udl/eps/softarch/academicrecruit/domain/Qualifications.java
+++ src/main/java/cat/udl/eps/softarch/academicrecruit/domain/Qualifications.java
@@ -7,22 +7,19 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Value;
 
 @Entity
 @Data
-@Value
+@EqualsAndHashCode(callSuper = true)
 public class Qualifications extends UriEntity<Long>{
-
     @GeneratedValue(strategy = GenerationType.AUTO)
-
     @Id
     private Long id;
-
     @NotBlank
     @Min(0)
     @Max(10)
     private Float mark;
-
     private String observation;
 }
--- src/test/java/cat/udl/eps/softarch/academicrecruit/steps/QualificationsStepsDefs.java
+++ src/test/java/cat/udl/eps/softarch/academicrecruit/steps/QualificationsStepsDefs.java
@@ -3,7 +3,7 @@ package cat.udl.eps.softarch.academicrecruit.steps;
 import cat.udl.eps.softarch.academicrecruit.domain.Qualifications;
 import cat.udl.eps.softarch.academicrecruit.domain.User;
 import cat.udl.eps.softarch.academicrecruit.repository.AdminRepository;
-import cat.udl.eps.softarch.academicrecruit.repository.Qualifications;
+import cat.udl.eps.softarch.academicrecruit.repository.QualificationsRepository;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
@@ -21,32 +21,22 @@ public class QualificationsStepsDefs {
     final QualificationsRepository qualificationsRepository;
 
     QualificationsStepsDefs(StepDefs stepDefs, QualificationsRepository qualificationsRepository) {
-        this.stepsDefs = stepsDefs;
+        this.stepDefs = stepDefs;
         this.qualificationsRepository = qualificationsRepository;
     }
 
-    @When("I set a new qualification mark {int} and an observation {string}")
-    public void iRegisterANewQualificationMarkAndObservation(int mark, String observation) throws Exception {
+    @When("I set a new qualification mark {string} and an observation {string}")
+    public void iRegisterANewQualificationMarkAndObservation(String mark, String observation) throws Exception {
         Qualifications qualifications = new Qualifications();
-        qualifications.setMark(mark);
+        qualifications.setMark(Float.valueOf(mark));
         qualifications.setObservation(observation);
 
         stepDefs.result = stepDefs.mockMvc.perform(
             post("/qualifications")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(new JSONObject(stepDefs.mapper.writeValueAsString(job)).toString())
+                .content(new JSONObject(stepDefs.mapper.writeValueAsString(qualifications)).toString())
                 .accept(MediaType.APPLICATION_JSON)
                         .with(AuthenticationStepDefs.authenticate()))
                 .andDo(print());
-    }
-
-    @And("I can check that the mark {int} is correct")
-    public void iCanCheckThatTheMarkIsCorrect(int mark) {
-
-    }
-
-    @And("I can check that the observation {string} is correct")
-    public void iCanCheckThatTheObservationIsCorrect(String observation) {
-
     }
 }
