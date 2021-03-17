2d3d641db762b983c8e5b2434dd833cd709157cb
Paula (Latra) Gallucci <personadelmonton@gmail.com>
2021-03-17 18:45:45 +0100

    Avoided using api call after get item by name

--- src/test/java/cat/udl/eps/softarch/academicrecruit/steps/PhaseStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/academicrecruit/steps/PhaseStepDefs.java
@@ -9,6 +9,7 @@ import org.json.JSONObject;
 import org.junit.Assert;
 import org.springframework.http.MediaType;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -55,22 +56,22 @@ public class PhaseStepDefs {
 
     @And("It has been created a new phase with number {int} name {string}, initialDate {string} and finishDate {string}")
     public void itHasBeenCreatedANewPhaseWithNumberNameInitialDateAndFinishDate(int number, String name, String initialDate, String finalDate) throws  Exception {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         List<Phase> phases = phaseRepository.findByNameContaining(name);
-        stepDefs.result = stepDefs.mockMvc.perform(
-                get("/phases/{phases}", phases.get(0).getId())
-                        .accept(MediaType.APPLICATION_JSON)
-                        .with(AuthenticationStepDefs.authenticate()))
-                .andDo(print())
-                .andExpect(jsonPath("$.phaseNumber", is(number)))
-                .andExpect(jsonPath("$.name", is(name)))
-                .andExpect(jsonPath("$.initialDate").exists())
-                .andExpect(jsonPath("$.finalDate").exists())
-        ;
+        if (phases.size() > 0) {
+            Phase phase = phases.get(0);
+            Assert.assertEquals(name, phase.getName());
+            Assert.assertEquals(initialDate, dateFormat.format(phase.getInitialDate()));
+            Assert.assertEquals(finalDate, dateFormat.format(phase.getFinalDate()));
+        }
+        else {
+            Assert.fail();
+        }
     }
 
     @And("It phase with name {string} has not been created.")
     public void itPhaseWithNameHasNotBeenCreated(String arg0) {
         List<Phase> phaseList = phaseRepository.findByNameContaining(arg0);
-        Assert.assertTrue(phaseList.size() == 0);
+        Assert.assertEquals(0, phaseList.size());
     }
 }
